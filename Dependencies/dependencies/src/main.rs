f940ed55de69ce7154f1825e6b426e100c6b3271
Fabio Schunig <fabio.schunig@gmail.com>
2025-01-10 23:36:47 -0300

    Rust - Using function say to print a nice Rustaceans message

--- Dependencies/dependencies/src/main.rs
+++ Dependencies/dependencies/src/main.rs
@@ -1,5 +1,11 @@
 use ferris_says::say;
+use std::io::{stdout, BufWriter};
 
 fn main() {
-    println!("Rust - Dependencies project");
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
